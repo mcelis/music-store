c3a94fe5bc70aabdbca745d6cb828882bcfdbf3a
Mark <marco.celis@gmail.com>
2021-05-25 22:01:49 -0500

    ajuste visual logout y img de banner

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class BannerComponent implements OnInit {
 
   images:string[] = [
-
+    './assets/images/banner-1.jpg',
+    './assets/images/banner-2.jpg',
+    './assets/images/banner-3.jpg',
   ];
   constructor() { }
 
